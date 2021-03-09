9672b7a79fa0905d7abdc131e882ea11b6f36997
Selikhova Kate <selikhovakate@gmail.com>
2021-03-09 13:29:24 +0300

    modificate, improve original function

--- module_0(try_number).py
+++ module_0(try_number).py
@@ -1,62 +1,33 @@
-import random
+import numpy as np # импортируем бибилиотеку, которая позволяет обрабатывать большие массивы данных
 
-def count_of_tries (a, b):
-    number = random.randint(a, b+1)
-    count = 0
-    mid_range = round(b/2)
-
-    while mid_range != number:
-        count += 1
-        if mid_range > number:
-            b = mid_range
+def score_game(game_core):
+    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1,101, size=(1000))
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls)) # расчитваем среднее арифметическое значение элементов списка 
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return(score)
+    
+def game_core_v3(number):
+    global a # назначем глобальную переменную, чтобы она была видна за пределами функции
+    a = 1 # нижняя граница диапазона
+    global b # назначем глобальную переменную, чтобы она была видна за пределами функции
+    b = 100 # верхняя граница диапазон
+    count = 1 # назначаем счетчик, который считает количество итераций
+    predict = np.random.randint(a, b) # с помощью библиотеки random выбираем нужное целое число (integer) в заданном диапазоне 
+    mid_range = round(b/2) # для бинарного поиска назначаем переменную - середину диапазона
+    while mid_range != predict: # задаем условие, при котором цикл будет выполняться
+        if mid_range > predict: 
+            b = mid_range # сужаем диапазон поиска наполовину, до или после середины списка. 
+                          # в этом случае от 1 до 50, тк диапазон от 1 до 100
         else:
-            a = mid_range
-        
-        mid_range = a + round((b - a) / 2)
-    
-    return (count)
-    
-print (count_of_tries (1, 100))
+            a = mid_range # сужаем диапазон поиска наполовину, до или после середины списка. 
+                          # в этом случае от 50 до 100, тк диапазон от 1 до 100
+        mid_range = a + round((b - a) / 2) # рассчитываем середину сокращенного диапазона
+        count += 1 
+    return(count) 
 
-# import numpy as np
-
-# def game_core_v1(number):
-#     '''Просто угадываем на random, никак не используя информацию о больше или меньше.
-#        Функция принимает загаданное число и возвращает число попыток'''
-#     count = 0
-#     while True:
-#         count+=1
-#         predict = np.random.randint(1,101) # предполагаемое число
-#         if number == predict: 
-#         return (count) # выход из цикла, если угадали
-
-        
-# def score_game(game_core):
-#     '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
-#     count_ls = []
-#     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
-#     random_array = np.random.randint(1,101, size=(1000))
-#     for number in random_array:
-#         count_ls.append(game_core(number))
-#     score = int(np.mean(count_ls))
-#     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
-    
-#     return (score)
-
-# print (score_game(game_core_v1))
-
-# def game_core_v2(number):
-#     '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
-#        Функция принимает загаданное число и возвращает число попыток'''
-# count = 1
-# predict = np.random.randint(1,101)
-# while number != predict:
-#     count+=1
-#     if number > predict: 
-#         predict += 1
-#     elif number < predict: 
-#         predict -= 1
-        
-#     return(count) # выход из цикла, если угадали
-
-# print (score_game(game_core_v2))
+print (score_game(game_core_v3))
